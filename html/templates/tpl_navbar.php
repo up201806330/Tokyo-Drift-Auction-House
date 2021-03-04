884242f0451c7c034886714f4eb9a1f77624419a
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-04 15:37:26 +0000

    Navbar Fixed. 1st "Draft" Complete

--- html/templates/tpl_navbar.php
+++ html/templates/tpl_navbar.php
@@ -3,43 +3,40 @@
      * Draws navbar
      */
     ?>
-
-<nav class="navbar navbar-expand-md navbar-dark bg-navbar">
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark bg-navbar">
   <div class="container-fluid">
     <!-- <a href="main.php"></a> -->
     <a class="navbar-brand" href="#">
         <img class="logo" src="./logo.svg" width="80" height="80">
         Tokyo Drift Auction House
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    
-      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="#">Explore Auctions</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Create new auction</a>
-        </li>
         <li class="nav-item">
           <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">About / Contacts</a>
         </li>
-
-      </ul>
-
-      <ul class="navbar-nav mx-auto mb-2 mb-lg-0 d-flex">
-            <li>
-                <a class="nav-link left-vert-bar" href="#">Log in</a>
-            </li>
-            <li>
-                <a class="nav-link" href="#">Sign up</a>
-            </li>
         </ul>
+        <ul class="navbar-nav ms-auto">
+          <li>
+            <button class="nav-link btn" href="#">Log in</button>
+          </li>
+          <span class="left-vert-bar"></span>
+          <li>
+            <button class="nav-link btn" href="#">Sign up</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
 </nav>
 
+</header>
 <?php } ?>
