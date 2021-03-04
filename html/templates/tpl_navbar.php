36b964a5226f2ced13c9cc803d1bed448a88bf63
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-04 01:47:56 +0000

    Text Elements and Logo Added to Navbar (Margins/Paddings to be changed)

---
+++ html/templates/tpl_navbar.php
@@ -0,0 +1,48 @@
+<?php function draw_navbar() {
+    /**
+     * Draws navbar
+     */
+    ?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a href="main.php">
+        
+    </a>
+    <a class="navbar-brand" href="#">
+        <img class="logo" src="./logo.svg" width="80" height="80">
+        Tokyo Drift Auction House
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Explore Auctions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Create new auction</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">About / Contacts</a>
+        </li>
+
+      </ul>
+      <!-- <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
+            <li>
+                <a class="nav-link" href="#">Log in</a>
+            </li>
+            <li>
+                <a class="nav-link" href="#">Sign up</a>
+            </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+
+<?php } ?>
