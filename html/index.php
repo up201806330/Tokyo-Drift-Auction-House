4e257da054c22df8c0c422585330c6a33e518a0f
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-05 19:56:54 +0000

    Background Image and Search Box Added

--- html/index.php
+++ html/index.php
@@ -2,12 +2,27 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>Title of the document</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./css/homepage.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+
+    <title>Homepage</title>
   </head>
   <body>
-    <p>Content from HTML.</p>
   <?php
-    echo '<p>Content from PHP.</p>';
+    include_once("./pages/homepage.php");
+
+    draw_homepage();
   ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ html/pages/homepage.php
@@ -0,0 +1,22 @@
+<?php function draw_homepage() {
+    /**
+     * Draws homepage
+     */
+    ?>
+
+<div class="container">
+    <div class="overlay"></div>
+    <div class="homepage-bg-top">
+        <div class="col-md-6 col-lg-6 col-11 mx-auto my-auto search-box">
+            <div class="input-group form-container">
+                <input type="text" name="search" class="form-control search-input" placeholder="Keyword" autofocus="autofocus" autocomplete=off>
+                <span class="input-group-btn">
+
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php } ?>
