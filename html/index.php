04d8d7233621f74ec8dfce8152dc092516a04bd1
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-05 02:06:29 +0000

    First Draft of Footer Complete. bg-color and Alignment of social links in progress

--- html/index.php
+++ html/index.php
@@ -2,12 +2,27 @@
 <html>
   <head>
     <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./css/tpl_footer.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+
     <title>Title of the document</title>
   </head>
   <body>
-    <p>Content from HTML.</p>
   <?php
-    echo '<p>Content from PHP.</p>';
+    include_once("./templates/tpl_footer.php");
+
+    draw_footer();
   ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ html/templates/tpl_footer.php
@@ -0,0 +1,66 @@
+<?php function draw_footer() {
+    /**
+     * Draws navbar
+     */
+    ?>
+
+<footer class="bg-footer text-white pt-3 pb-2">
+
+    <div class="container text-center text-md-left">
+
+        <div class="row text-center text-md-left">
+
+            <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mt-5 mb-4">
+
+                <p class="mb-0">Copyright © 2021 Tokyo Drift Inc.</p>
+                <p>All Rights Reserved</p>
+            
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-4 mb-4">
+                <h5 class="mb-4 mt-4 font-weight-bold text-white">Terms of Service</p>
+            </div>
+
+            <div class="cold-md-3 col-lg-2 col-xl-3 mx-auto">
+                <h5 class="mb-2 font-weight-bold text-white">Find Us</h5>
+
+                <div class="container">
+                    <div class="row mb-1">
+                        <div class="col">
+                            <a href="#" target="_blank" class="btn-social btn-facebook">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                            Facebook
+                        </div>
+                        <div class="col justify-content-start">
+                            <a href="#" target="_blank" class="btn-social btn-youtube">
+                                <i class="fa fa-youtube"></i>
+                            </a>
+                            Youtube
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <a href="#" target="_blank" class="btn-social btn-twitter">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                            Twitter
+                        </div>
+                        <div class="col">
+                            <a href="#" target="_blank" class="btn-social btn-instagram">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                            Instagram
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
+<?php } ?>
