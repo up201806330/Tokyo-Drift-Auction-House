2d5f6ad9440757b0d2077e9d0b6f5a259fea62d6
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-05 12:46:48 +0000

    Changed Logo Color and Font Weight; Added slight hover effect

--- html/templates/tpl_navbar.php
+++ html/templates/tpl_navbar.php
@@ -7,8 +7,8 @@
   <nav class="navbar navbar-expand-md navbar-dark bg-navbar">
   <div class="container-fluid">
     <!-- <a href="main.php"></a> -->
-    <a class="navbar-brand text-white" href="#">
-      <img class="logo" src="./logo.svg" width="80" height="80">
+    <a class="navbar-brand text-white navbar-content-bold" href="#">
+      <img class="logo filter-logo" src="./logo.svg" width="80" height="80">
       Tokyo Drift Auction House
     </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,21 +17,21 @@
       <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link text-white" aria-current="page" href="#">Explore Auctions</a>
+            <a class="nav-link text-white navbar-content-bold" aria-current="page" href="#">Explore Auctions</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Create new auction</a>
+            <a class="nav-link text-white navbar-content-bold" href="#">Create new auction</a>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#" tabindex="-1" aria-disabled="true">About / Contacts</a>
+            <a class="nav-link text-white navbar-content-bold" href="#" tabindex="-1" aria-disabled="true">About / Contacts</a>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
           <li>
-            <button class="nav-link btn text-white" href="#">Log in</button>
+            <button class="nav-link btn text-white navbar-content-bold" href="#">Log in</button>
           </li>
           <span class="left-vert-bar"></span>
           <li>
-            <button class="nav-link btn text-white" href="#">Sign up</a>
+            <button class="nav-link btn text-white navbar-content-bold" href="#">Sign up</a>
           </li>
         </ul>
       </div>
