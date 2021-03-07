00ad70e56e921d70cf9554057b1a97a98b70bc11
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-07 13:30:53 +0000

    Fixed Image Size Scaling with different images resolutions. Card Complete.

--- html/pages/homepage.php
+++ html/pages/homepage.php
@@ -29,16 +29,16 @@
             <div class="row px-5 mx-auto">
 
                 <!-- Card -->
-                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3">
+                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
                     <div class="card">
                         
                         <img src="../assets/supercar-homepage-bg.jpg" alt="..." class="card-img-to rounded">
 
                         <!-- Card Body -->
-                        <div class="card-body">
-                            <h4 class="card-title">Brand</h4>
-                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hrs</span> </p>
-                            <p class="card-text">21.000€</p>
+                        <div class="card-body pt-1">
+                            <h4 class="card-title text-center">Brand 1</h4>
+                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
+                            <p class="card-text">Currently at 21.000€</p>
                             <a href="#" class="stretched-link"></a> <!-- Card as a link -->
                         </div>
                         <!-- End of Card Body -->
@@ -47,16 +47,16 @@
                 <!-- End of Card -->
 
                 <!-- Card -->
-                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3">
+                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
                     <div class="card">
                         
                         <img src="../assets/supercar-homepage-bg.jpg" alt="..." class="card-img-to rounded">
 
                         <!-- Card Body -->
-                        <div class="card-body">
-                            <h4 class="card-title">Brand</h4>
-                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hrs</span> </p>
-                            <p class="card-text">21.000€</p>
+                        <div class="card-body pt-1">
+                            <h4 class="card-title text-center">Brand 2</h4>
+                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
+                            <p class="card-text">Currently at 21.000€</p>
                             <a href="#" class="stretched-link"></a> <!-- Card as a link -->
                         </div>
                         <!-- End of Card Body -->
@@ -65,16 +65,16 @@
                 <!-- End of Card -->
 
                 <!-- Card -->
-                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3">
+                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
                     <div class="card">
                         
                         <img src="../assets/supercar-homepage-bg.jpg" alt="..." class="card-img-to rounded">
 
                         <!-- Card Body -->
-                        <div class="card-body">
-                            <h4 class="card-title">Brand</h4>
-                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hrs</span> </p>
-                            <p class="card-text">21.000€</p>
+                        <div class="card-body pt-1">
+                            <h4 class="card-title text-center">Brand 3</h4>
+                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
+                            <p class="card-text">Currently at 21.000€</p>
                             <a href="#" class="stretched-link"></a> <!-- Card as a link -->
                         </div>
                         <!-- End of Card Body -->
@@ -83,16 +83,16 @@
                 <!-- End of Card -->
 
                 <!-- Card -->
-                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3">
+                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
                     <div class="card">
                         
                         <img src="../assets/supercar-2.jpg" alt="..." class="card-img-to rounded">
 
                         <!-- Card Body -->
-                        <div class="card-body">
-                            <h4 class="card-title">Brand</h4>
-                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hrs</span> </p>
-                            <p class="card-text">21.000€</p>
+                        <div class="card-body pt-1">
+                            <h4 class="card-title text-center">Brand 4</h4>
+                            <p class="card-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
+                            <p class="card-text">Currently at 21.000€</p>
                             <a href="#" class="stretched-link"></a> <!-- Card as a link -->
                         </div>
                         <!-- End of Card Body -->
