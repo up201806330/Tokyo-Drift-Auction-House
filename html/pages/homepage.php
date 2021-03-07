4f505fec339450586c6256daaba28c3e198467f2
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-07 17:07:12 +0000

    Reorganized auction card template in different files (php / css)

--- html/pages/homepage.php
+++ html/pages/homepage.php
@@ -27,60 +27,13 @@
         <!-- Container For All Displayed Auctions -->
         <div class="container">
             <div class="row px-5 mx-auto">
+                <?php
+                    include_once("./templates/tpl_auction_card.php");
 
-                <!-- Card -->
-                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
-                    <div class="card">
-                        
-                        <img src="../assets/supercar-homepage-bg.jpg" alt="..." class="card-img-to rounded car-img-thumbnail">
-
-                        <!-- Card Body -->
-                        <div class="card-body pt-1">
-                            <h4 class="card-title text-center">Brand 1</h4>
-                            <p class="card-text time-remaining-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
-                            <p class="card-text current-value">Currently at 21.000€</p>
-                            <a href="#" class="stretched-link"></a> <!-- Card as a link -->
-                        </div>
-                        <!-- End of Card Body -->
-                    </div>
-                </div>
-                <!-- End of Card -->
-
-                <!-- Card -->
-                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
-                    <div class="card">
-                        
-                        <img src="../assets/supercar-homepage-bg.jpg" alt="..." class="card-img-to rounded car-img-thumbnail">
-
-                        <!-- Card Body -->
-                        <div class="card-body pt-1">
-                            <h4 class="card-title text-center">Brand 2</h4>
-                            <p class="card-text time-remaining-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
-                            <p class="card-text current-value">Currently at 21.000€</p>
-                            <a href="#" class="stretched-link"></a> <!-- Card as a link -->
-                        </div>
-                        <!-- End of Card Body -->
-                    </div>
-                </div>
-                <!-- End of Card -->
-
-                <!-- Card -->
-                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
-                    <div class="card">
-                        
-                        <img src="../assets/supercar-homepage-bg.jpg" alt="..." class="card-img-to rounded car-img-thumbnail">
-
-                        <!-- Card Body -->
-                        <div class="card-body pt-1">
-                            <h4 class="card-title text-center">Brand 3</h4>
-                            <p class="card-text time-remaining-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
-                            <p class="card-text current-value">Currently at 21.000€</p>
-                            <a href="#" class="stretched-link"></a> <!-- Card as a link -->
-                        </div>
-                        <!-- End of Card Body -->
-                    </div>
-                </div>
-                <!-- End of Card -->
+                    draw_auction_card();
+                    draw_auction_card();
+                    draw_auction_card();
+                ?>
 
                 <!-- Card -->
                 <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
---
+++ html/templates/tpl_auction_card.php
@@ -0,0 +1,26 @@
+<?php function draw_auction_card() {
+    /**
+     * Draws an auction card
+     */
+    ?>
+            <link rel="stylesheet" href="../css/tpl_auction_card.css" type="text/css">
+            
+                <!-- Card -->
+                <div class="col-12 col-md-6 col-lg-3 mx-auto mb-3 card-container">
+                    <div class="card">
+                        
+                        <img src="../assets/supercar-homepage-bg.jpg" alt="..." class="card-img-to rounded car-img-thumbnail">
+
+                        <!-- Card Body -->
+                        <div class="card-body pt-1">
+                            <h4 class="card-title text-center">Brand 1</h4>
+                            <p class="card-text time-remaining-text"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">3 hours remaining</span> </p>
+                            <p class="card-text current-value">Currently at 21.000€</p>
+                            <a href="#" class="stretched-link"></a> <!-- Card as a link -->
+                        </div>
+                        <!-- End of Card Body -->
+                    </div>
+                </div>
+                <!-- End of Card -->
+
+<?php } ?>
