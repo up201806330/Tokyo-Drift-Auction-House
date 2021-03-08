7faeb8d35c78b4330ac14124feaef8813eaae089
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-03-08 15:41:09 +0000

    Started sidebar ig

--- html/pages/search.php
+++ html/pages/search.php
@@ -1,6 +1,7 @@
 <?php
     include_once('templates/tpl_search.php');
 
+    draw_topbar();
     draw_sidebar();
 
 ?>
--- html/templates/tpl_search.php
+++ html/templates/tpl_search.php
@@ -1,8 +1,8 @@
-<?php function draw_sidebar(){ ?>
+<?php function draw_topbar(){ ?>
 
-<div class="container-fluid">
+<div class="container-fluid bg-light">
   <div class="row">
-    <div class="col-sm-4 mb-3">
+    <div class="col-sm-4 pt-5">
       <div class="display-2">
         Browse Auctions
       </div class="display-2">
@@ -19,7 +19,6 @@
         <div class="p-5 col-4 col-sm-6">
           <label for="selectOrderResults" class="form-label">Order By</label>
           <select class="form-select" id="selectOrderResults" aria-label="Select order of results">
-            <option selected>Ascending Current Bid</option>
             <option value="1">Ascending Current Bid</option>
             <option value="2">Descending Current Bid</option>
             <option value="3">Ending Sooner</option>
@@ -33,3 +32,67 @@
 
 <?php } ?>
 
+<?php function draw_sidebar(){ ?>
+
+  <div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item pt-3">
+            <div class="display-6">
+              Filter results
+            </div class="display-6">
+          </li>
+          
+          <li class="nav-item pt-5">
+            <label for="selectConditionFilter" class="form-label">Condition</label>
+            <select class="form-select" id="selectConditionFilter" aria-label="Filter by condition">
+              <option value="1">Mint</option>
+              <option value="2">Clean or better</option>
+              <option value="3">Average or better</option>
+              <option value="4">Rough or better</option>
+            </select>
+          </li>
+          
+          <li class="nav-item pt-5">
+            <div class="btn-group dropend">
+              <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                Categories
+              </button>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="checkboxSports" checked>
+                    <label class="form-check-label" for="checkboxSports">
+                      Sports
+                    </label>
+                  </div>
+                </a></li>
+                <li><a class="dropdown-item" href="#">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="checkboxAntiques" checked>
+                    <label class="form-check-label" for="checkboxAntiques">
+                      Antiques
+                    </label>
+                  </div>
+                </a></li>
+                <li><a class="dropdown-item" href="#">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="checkboxFamily" checked>
+                    <label class="form-check-label" for="checkboxFamily">
+                      Family
+                    </label>
+                  </div>
+                </a></li>
+              </ul>
+            </div>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  </div>
+</div>
+
+<?php } ?>
