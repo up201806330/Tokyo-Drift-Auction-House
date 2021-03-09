3eceb9417a899f74b566ec39bdebe7ac7fb88ac9
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-03-09 00:22:28 +0000

    progress ig

--- html/pages/search.php
+++ html/pages/search.php
@@ -1,7 +1,8 @@
 <?php
     include_once('templates/tpl_search.php');
+    include_once('templates/tpl_auction_card.php');
 
     draw_topbar();
     draw_sidebar();
-
+    draw_auction_card();
 ?>
--- html/templates/tpl_search.php
+++ html/templates/tpl_search.php
@@ -2,21 +2,21 @@
 
 <div class="container-fluid bg-light">
   <div class="row">
-    <div class="col-sm-4 pt-5">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-6 pt-5">
       <div class="display-2">
         Browse Auctions
-      </div class="display-2">
+      </div>
     </div>
-    <div class="col-sm-8 d-flex align-items-center overflow-hidden">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-6 ">
       <div class="row mx-auto gx-4">
-        <div class="p-5 col-8 col-sm-6">
+        <div class="pt-5 col-12 col-sm-6">
           <label for="searchByTags" class="form-label">Search Tags</label>
           <div class="input-group" id="searchByTags">
             <input type="text" class="form-control" placeholder="Try some keywords" aria-label="Input search keywords" aria-describedby="button-search-1">
             <button class="btn btn-outline-secondary" type="button" id="button-search-1">Search</button>
           </div>
         </div>
-        <div class="p-5 col-4 col-sm-6">
+        <div class="pt-5 pb-4 col-12 col-sm-6">
           <label for="selectOrderResults" class="form-label">Order By</label>
           <select class="form-select" id="selectOrderResults" aria-label="Select order of results">
             <option value="1">Ascending Current Bid</option>
@@ -86,6 +86,27 @@
                   </div>
                 </a></li>
               </ul>
+            </div>
+          </li>
+
+          <!-- <li class="nav-item pt-5">
+            Insert multi range slider for horsepower here
+          </li>
+          <li class="nav-item pt-5">
+            Insert multi range slider manufacture year here
+          </li> -->
+          
+          <li class="nav-item pt-5">
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" id="switchShowUsedCars" checked>
+              <label class="form-check-label" for="switchShowUsedCars">Show Used Cars</label>
+            </div>
+          </li>
+          
+          <li class="nav-item pt-5">
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" id="switchFinalizedAuctions">
+              <label class="form-check-label" for="switchFinalizedAuctions">Show Finalized Auctions</label>
             </div>
           </li>
 
