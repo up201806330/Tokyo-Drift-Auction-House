96926797183d860449d860c45407c4549dade7ec
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-03-09 16:19:34 +0000

    Side bar completed ; need to adjust card placement

--- html/pages/search.php
+++ html/pages/search.php
@@ -3,6 +3,9 @@
     include_once('templates/tpl_auction_card.php');
 
     draw_topbar();
+    open_main();
     draw_sidebar();
     draw_auction_card();
+    close_main();
+    
 ?>
--- html/templates/tpl_search.php
+++ html/templates/tpl_search.php
@@ -1,3 +1,17 @@
+<?php function open_main() { ?>
+
+<div class="container-fluid" style="flex: auto">
+<div class="row h-100">
+
+<?php } ?>
+
+<?php function close_main() { ?>
+
+</div></div>
+
+<?php } ?>
+
+
 <?php function draw_topbar(){ ?>
 
 <div class="container-fluid bg-light">
@@ -34,86 +48,125 @@
 
 <?php function draw_sidebar(){ ?>
 
-  <div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item pt-3">
-            <div class="display-6">
-              Filter results
-            </div class="display-6">
-          </li>
-          
-          <li class="nav-item pt-5">
-            <label for="selectConditionFilter" class="form-label">Condition</label>
-            <select class="form-select" id="selectConditionFilter" aria-label="Filter by condition">
-              <option value="1">Mint</option>
-              <option value="2">Clean or better</option>
-              <option value="3">Average or better</option>
-              <option value="4">Rough or better</option>
-            </select>
-          </li>
-          
-          <li class="nav-item pt-5">
-            <div class="btn-group dropend">
-              <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                Categories
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="checkboxSports" checked>
-                    <label class="form-check-label" for="checkboxSports">
-                      Sports
-                    </label>
-                  </div>
-                </a></li>
-                <li><a class="dropdown-item" href="#">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="checkboxAntiques" checked>
-                    <label class="form-check-label" for="checkboxAntiques">
-                      Antiques
-                    </label>
-                  </div>
-                </a></li>
-                <li><a class="dropdown-item" href="#">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="checkboxFamily" checked>
-                    <label class="form-check-label" for="checkboxFamily">
-                      Family
-                    </label>
-                  </div>
-                </a></li>
-              </ul>
-            </div>
-          </li>
 
-          <!-- <li class="nav-item pt-5">
-            Insert multi range slider for horsepower here
-          </li>
-          <li class="nav-item pt-5">
-            Insert multi range slider manufacture year here
-          </li> -->
-          
-          <li class="nav-item pt-5">
-            <div class="form-check form-switch">
-              <input class="form-check-input" type="checkbox" id="switchShowUsedCars" checked>
-              <label class="form-check-label" for="switchShowUsedCars">Show Used Cars</label>
-            </div>
-          </li>
-          
-          <li class="nav-item pt-5">
-            <div class="form-check form-switch">
-              <input class="form-check-input" type="checkbox" id="switchFinalizedAuctions">
-              <label class="form-check-label" for="switchFinalizedAuctions">Show Finalized Auctions</label>
-            </div>
-          </li>
+<nav id="sidebarMenu" class="col-12 col-sm-12 col-md-12 col-lg-3 d-md-block bg-light sidebar collapse show">
+  <div class="position-sticky pt-3">
+    <ul class="nav flex-column">
+      <li class="nav-item pt-3">
+        <div class="display-6">
+          Filter results
+        </div class="display-6">
+      </li>
+      
+      <li class="nav-item pt-5">
+        <label for="selectConditionFilter" class="form-label">Condition</label>
+        <select class="form-select" id="selectConditionFilter" aria-label="Filter by condition">
+          <option value="1">Mint</option>
+          <option value="2">Clean or better</option>
+          <option value="3">Average or better</option>
+          <option value="4">Rough or better</option>
+        </select>
+      </li>
+      
+      <li class="nav-item pt-5">
+        <div class="btn-group dropend">
+          <button type="button" class="btn-lg btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            Categories
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="checkboxSports" checked>
+                <label class="form-check-label" for="checkboxSports">
+                  Sports
+                </label>
+              </div>
+            </a></li>
+            <li><a class="dropdown-item" href="#">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="checkboxAntiques" checked>
+                <label class="form-check-label" for="checkboxAntiques">
+                  Antiques
+                </label>
+              </div>
+            </a></li>
+            <li><a class="dropdown-item" href="#">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="checkboxFamily" checked>
+                <label class="form-check-label" for="checkboxFamily">
+                  Family
+                </label>
+              </div>
+            </a></li>
+          </ul>
+        </div>
+      </li>
 
-        </ul>
-      </div>
-    </nav>
+      <li class="nav-item pt-5">
+        <label class="form-slider-label" for="multiRangeHorsepower">Horsepower (HP)</label>
+        <?php draw_multi_range_slider("multiRangeHorsepower", 0, 100); ?>
+      </li>
+      
+      <li class="nav-item pt-5">
+        <label class="form-slider-label" for="multiRangeYear">Year of manufacture</label>
+        <?php draw_multi_range_slider("multiRangeYear", 0, 100); ?>
+      </li>
+
+      <li class="nav-item pt-5">
+        <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" id="switchShowUsedCars" checked>
+          <label class="form-check-label" for="switchShowUsedCars">Show Used Cars</label>
+        </div>
+      </li>
+      
+      <li class="nav-item pt-5">
+        <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" id="switchFinalizedAuctions">
+          <label class="form-check-label" for="switchFinalizedAuctions">Show Finalized Auctions</label>
+        </div>
+      </li>
+
+    </ul>
   </div>
+</nav>
+
+
+<?php } ?>
+
+<!-- From https://codepen.io/glitchworker/pen/XVdKqj -->
+<?php function draw_multi_range_slider($id, $min, $max) { ?>
+
+<div slider id="<?=$id?>">
+  <div>
+    <div inverse-left style="width:70%;"></div>
+    <div inverse-right style="width:70%;"></div>
+    <div range style="left:30%;right:40%;"></div>
+    <span thumb style="left:30%;"></span>
+    <span thumb style="left:60%;"></span>
+    <div sign style="left:30%;">
+      <span id="value">30</span>
+    </div>
+    <div sign style="left:60%;">
+      <span id="value">60</span>
+    </div>
+  </div>
+  <input type="range" tabindex="0" value="30" max="<?=$max?>" min="<?=$min?>" step="1" oninput="
+  this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
+  var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
+  var children = this.parentNode.childNodes[1].childNodes;
+  children[1].style.width=value+'%';
+  children[5].style.left=value+'%';
+  children[7].style.left=value+'%';children[11].style.left=value+'%';
+  children[11].childNodes[1].innerHTML=this.value;" />
+
+  <input type="range" tabindex="0" value="60" max="<?=$max?>" min="<?=$min?>" step="1" oninput="
+  this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));
+  var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
+  var children = this.parentNode.childNodes[1].childNodes;
+  children[3].style.width=(100-value)+'%';
+  children[5].style.right=(100-value)+'%';
+  children[9].style.left=value+'%';children[13].style.left=value+'%';
+  children[13].childNodes[1].innerHTML=this.value;" />
 </div>
 
 <?php } ?>
