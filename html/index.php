2bc8ffc1b52d21f4a6640436e8b7b82858b3ca11
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-03-11 11:10:10 +0000

    Reverted

--- html/index.php
+++ html/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Title of the document</title>
-  </head>
-  <body>
-  <?php
-    include("pages/auction_page.php");
-  ?>
-  </body>
-</html>
+<?php
+  header('Location: pages/homepage.php');
+?>
