fb9bf61f9bb9d34d573b7517d5209fe93bb6e6da
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-03-11 15:52:13 +0000

    about page

--- html/templates/tpl_about.php
+++ html/templates/tpl_about.php
@@ -1,11 +1,24 @@
 <?php function draw_about() { ?>
 
-    <div class="container-fluid p-4">
+    <div class="container-fluid p-5">
         <div class="d-flex justify-content-center">
-            <img class="logo" src="../logo.svg" width="400" height="400">
+            <img class="logo img-fluid" src="../logo.svg" width="400" height="400">
         </div>
         
+        <div class="fw-light fs-1 pt-3 pb-2 text-center"><i><b>The future is now...</b></i></div>
+        <div class="fw-light fs-2 pt-3 pb-3 text-center"><i>Fill your garage with the cars from your dreams, any place, any time</i></div>
 
+
+        <div class="row mt-5 mb-5">
+            <div class="col-lg-6 col-md-12 pt-5">
+                <div class="fw-light fs-2 p-3 text-end"><b>Come visit us anytime! :))</b></div>
+                <div class="fw-light fs-3 pt-3 pe-3 text-end"><i>Dead End St. Nº420 Apt.69</i></div>
+                <div class="fw-light fs-3 pe-3 text-end"><b>255 889 232</b></div>
+            </div>
+            <div class="col-lg-6 col-md-12 ">
+                <img class="img-fluid rounded" src="../assets/mock_map.png">
+            </div>
+        </div>
     </div>
 
 
