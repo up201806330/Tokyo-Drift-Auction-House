c44f2bc17aac33d77bdff971f72917d69f9b3b51
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-03-11 18:03:06 +0000

    Stuff on mod page

--- html/templates/tpl_mod.php
+++ html/templates/tpl_mod.php
@@ -3,9 +3,13 @@ function draw_mod(){
 ?>
     <div class="container bg-light">
         <main class="mod col-md-8 mx-auto">
-            <h1>Manage user profiles</h1>
-            <h1>Manage auctions</h1>
-            <div class="row row-cols-3 justify-content-start cars-gallery overflow-auto pt-2">
+            <div class="display-3 pb-4 pt-5">
+                Manage user profiles
+            </div>
+            <div class="display-3 pb-4">
+                Manage auctions
+            </div>
+            <div class="row row-cols-3 justify-content-start cars-gallery overflow-auto p-4 rounded-3">
 <?php
 draw_auction_card_1();
 draw_auction_card_2();
