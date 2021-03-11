19cefcc4ce8dc5515966368cc045ca713930f07c
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-03-11 14:34:47 +0000

    Revert

--- html/index.php
+++ html/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Title of the document</title>
-  </head>
-  <body>
-  <?php
-      include("pages/create_auction.php");
-  ?>
-  </body>
-</html>
+<?php
+  header('Location: pages/homepage.php');
+?>
