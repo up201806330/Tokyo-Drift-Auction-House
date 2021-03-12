616b836ee75756a8d7ec8a31617d7ac2fd56cb4b
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-12 00:34:43 +0000

    Fixed profile branch merge issues

--- html/templates/tpl_navbar.php
+++ html/templates/tpl_navbar.php
@@ -30,6 +30,7 @@
   <link rel="stylesheet" href="../css/tpl_mod.css">
   <link rel="stylesheet" href="../css/login-overlay.css">
   <link rel="stylesheet" href="../css/sign-up.css">
+  <link rel="stylesheet" href="../css/profile.css">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
