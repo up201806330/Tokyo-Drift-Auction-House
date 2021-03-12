0f8c4836d65e50b75115dfe279db8ac9710b908c
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-12 19:33:46 +0000

    Footer Now Fully Responsive

--- html/templates/tpl_footer.php
+++ html/templates/tpl_footer.php
@@ -10,50 +10,56 @@
 
         <div class="row text-center text-md-left mx-auto">
 
-            <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mt-5 mb-4">
+            <div class="text-center col-md-3 col-lg-2 col-xl-3 my-auto mx-auto">
 
-                <p class="mb-0">Copyright © 2021 Tokyo Drift Inc.</p>
+                <p class="mt-1">Copyright © 2021 Tokyo Drift Inc.</p>
                 <p>All Rights Reserved</p>
             
             </div>
 
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 tos">
-                <h5 class="mb-4 mt-4 font-weight-bold "><a href="../pages/tos.php" class="nav-link text-white">Terms of Service</a></p>
+            <div class="col-md-2 col-lg-2 col-xl-3 mx-auto my-auto">
+                <h5 class="mb-4 mt-4 font-weight-bold"><a href="../pages/tos.php" class="nav-link text-white tos">Terms of Service</a></p>
             </div>
 
-            <div class="col-md-3 col-lg-2 col-xl-3 mx-auto  ">
-                <h5 class="mb-2 font-weight-bold text-white find-us ">Find Us</h5>
+            <div class="col-md-5 col-lg-5 col-xl-4 mx-auto">
 
-                <div class="container">
-                    <div class="row mb-1">
-                        <div class="col offset-2" align="left">
+                    <div class="row">
+                        <div class="col-xl-3 col-lg-3"></div>
+                        <div class="col-xl-4 col-lg-4 text-center">
+                            <h5 class="mb-2 font-weight-bold text-white text-center">Find Us</h5>
+                        </div>
+                        <div class="col-xl-3 col-lg-3"></div>
+                    </div>
+                    
+                    <div class="row find-us-row mb-1">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" align="left">
                             <a href="#" target="_blank" class="btn-social btn-facebook">
                                 <i class="fa fa-facebook"></i>
                             </a>
                             Facebook
                         </div>
-                        <div class="col" align="left">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" align="left">
                             <a href="#" target="_blank" class="btn-social btn-youtube">
                                 <i class="fa fa-youtube"></i>
                             </a>
                             Youtube
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col offset-2" align="left">
+                    <div class="row find-us-row">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" align="left">
                             <a href="#" target="_blank" class="btn-social btn-twitter">
                                 <i class="fa fa-twitter"></i>
                             </a>
                             Twitter
                         </div>
-                        <div class="col" align="left">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" align="left">
                             <a href="#" target="_blank" class="btn-social btn-instagram">
                                 <i class="fa fa-instagram"></i>
                             </a>
                             Instagram
                         </div>
                     </div>
-                </div>
+
 
             </div>
 
