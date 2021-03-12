77bf0803bcb99ddcf798ca61132c4cf8e20804b1
leonormsousa <leonormsousa@sapo.pt>
2021-03-12 12:55:12 +0000

    Create Auction Coeerent

--- html/pages/auction.php
+++ html/pages/auction.php
@@ -1,9 +1,9 @@
 <?php
     include_once("../templates/tpl_navbar.php");
-    include_once("../templates/tpl_create_auction.php");
+    include_once("../templates/tpl_auction.php");
     include_once("../templates/tpl_footer.php");
 
     draw_navbar();
-    draw_create_auction();
+    draw_auction();
     draw_footer();
 ?>
