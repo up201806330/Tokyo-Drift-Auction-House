9e0a60460e8ec1c0d2db68d3ad2ca329d70af51f
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-12 00:12:02 +0000

    Profile Page Almost Done

--- html/index.php
+++ html/index.php
@@ -8,25 +8,25 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./css/homepage.css">
+    <!-- <link rel="stylesheet" href="./css/homepage.css"> -->  <!-- TODO CSS CONFLICT HERE -->
     <link rel="stylesheet" href="./css/tpl_navbar.css">
     <link rel="stylesheet" href="./css/tpl_footer.css">
     <link rel="stylesheet" href="./css/profile.css">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/2fb51e88be.js" crossorigin="anonymous"></script>
 
     <title>Homepage</title>
   </head>
   <body>
   <?php
     include_once("./templates/tpl_navbar.php");
-    include_once("./pages/homepage.php");
+    // include_once("./pages/homepage.php");
     include_once("./templates/tpl_footer.php");
     include_once("./pages/profile.php");
     
     draw_navbar();
-    // draw_profile();
-    draw_homepage();
+    draw_profile();
+    // draw_homepage();
     draw_footer();
   ?>
 
