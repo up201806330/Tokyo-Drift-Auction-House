1cafe79c87cac2ee92fe5160f6456801a53fafd2
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-05-28 11:02:10 +0100

    Fixed #109

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,6 +36,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        $this->redirectTo = url()->previous();
         $this->middleware('guest')->except('logout');
     }
 
