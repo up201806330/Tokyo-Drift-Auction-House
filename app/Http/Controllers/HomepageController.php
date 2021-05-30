1bfff4b3305dbef48d348511964423cf6beaba79
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-05-30 12:47:38 +0100

    Added "Featured Category" in homepage

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -16,13 +16,26 @@ class HomepageController extends Controller
      */
     public function show() : View
     {
-        $auctionsForHomepage = $this->getRandomAuctions(3);
+        $categories = array('Sports', 'Antique', 'Family');
+        $rand_index = array_rand($categories, 1);
+        $category_name = $categories[$rand_index];
 
-        return view('pages.homepage', ['auctions' => $auctionsForHomepage]);
+        $fireDealsAuctionsForHomepage = $this->getRandomInProgressAuctions(3);
+        $featuredCategAuctionsForHomepage = $this->getRandomCatAuctions(3);
+
+        return view('pages.homepage', [
+            'fire_deals' => $fireDealsAuctionsForHomepage,
+            'featured_categ' => $featuredCategAuctionsForHomepage,
+            'category_name' => $category_name
+            ]);
     }
 
-    public function getRandomAuctions($nAuctions) : Collection {
-        return Auction::inRandomOrder()->limit($nAuctions)->get();
+    public function getRandomInProgressAuctions($nAuctions) : Collection {
+        return Auction::inRandomOrder()->limit($nAuctions)->where('auction.endingtime', '>', \Carbon\Carbon::now()->toDateString())->where('auction.startingtime', '<', \Carbon\Carbon::now()->toDateString())->get();
+    }
+
+    public function getRandomCatAuctions($nAuctions) : Collection {
+        return Auction::inRandomOrder()->limit($nAuctions)->where('auction.endingtime', '>', \Carbon\Carbon::now()->toDateString())->get();
     }
 
 }
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -31,7 +31,7 @@
                 <!-- Container For All Displayed Auctions -->
                 <main class="col ms-sm-auto pt-4 px-md-4">
                     <div class="row row row-cols-1 row-cols-sm-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 d-flex justify-content-center">
-                        @foreach ($auctions as $auction)
+                        @foreach ($fire_deals as $auction)
                             
                             @include('partials.auction_card', array(
                                 'id'            => $auction->id,
@@ -48,6 +48,36 @@
             </div>
         </div>
         <!-- End of Container For All Displayed Auctions -->
+    </div>
+
+    <!-- Featured Auctions Part of Homepage -->
+    <div class="homepage-bg-bottom">
+        <h4 class="full-res featured-cat-text text-md-start fs-1 text-white" style="float:left; padding-right: 0.4em !important;">Featured Category - </h4>
+        <h4 class="short-res featured-cat-text fs-1 text-white">Featured Category</h4>
+        <h4 class="featured-cat-text text-md-start fs-1 text-white">{{$category_name}}</h4>
+        
+        <div class="container-fluid" id="search-background" style="flex: auto">
+            <div class="row h-100">
+                <!-- Container For All Featured Auctions -->
+                <main class="col ms-sm-auto pt-4 px-md-4">
+                    <div class="row row row-cols-1 row-cols-sm-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 d-flex justify-content-center">
+                        @foreach ($featured_categ as $auction)
+                            
+                            @include('partials.auction_card', array(
+                                'id'            => $auction->id,
+                                'brand'         => $auction->vehicle->brand,
+                                'model'         => $auction->vehicle->model,
+                                'max_bid'       => $auction->getCurrentMaxBid(),
+                                'vehicle_imgs'  => $auction->getVehicleFromAuction(),
+                                'time_diff'     => $auction->getAdequateTimeDifference()
+                            ))
+                        @endforeach
+                    
+                    </div>
+                </main>
+                <!-- End of Container For All Featured Auctions -->
+            </div>
+        </div>
     </div>
 </div>
 
