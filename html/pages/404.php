e92895151f503c06a23ae54eb9174a00ca0a1a25
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-06-04 17:14:57 +0100

    Deleted Useless stuff

--- html/pages/404.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../templates/error_page.php';
-
-error_page(404, "Not Found");
-
-?>
--- html/pages/sign-up.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once("../templates/tpl_navbar.php");
-    include_once("../templates/tpl_sign-up.php");
-    include_once("../templates/tpl_footer.php");
-
-    draw_navbar();
-    draw_signup();
-    draw_footer();
-?>
