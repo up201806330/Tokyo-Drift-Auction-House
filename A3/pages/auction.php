3b03b638b5da5eaea43010571758565f021ccc7a
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-06-04 17:33:19 +0100

    Removed A3

--- A3/pages/auction.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once("../templates/tpl_navbar.php");
-    include_once("../templates/tpl_auction.php");
-    include_once("../templates/tpl_footer.php");
-
-    draw_navbar_logged_in();
-    draw_auction();
-    draw_footer();
-?>
