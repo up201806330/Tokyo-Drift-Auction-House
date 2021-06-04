7f911adc7d8f37b7e4e7178d9c66083d0f17d61e
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-06-04 16:09:45 +0100

    Fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,7 +56,7 @@ class LoginController extends Controller
     {
         $this->validateLogin($request);
 
-        if($request->user()->bannedAll()->exists()){
+        if($this->getUser()->bannedAll()->exists()){
             return new RedirectResponse(route('banned'));
         }
 
